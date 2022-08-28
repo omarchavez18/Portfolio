2ddba2b5a3ba52db9688d39721574ca9f0c62c93
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-08-28 15:04:38 -0500

    creaye modal function and close btn

--- app.js
+++ app.js
@@ -35,18 +35,56 @@ function createGallery() {
 
   // FOR THIS SECTION THE NAME OF YOUR IMAGES MOST BE NUMBERS = 1,2,3,4,5,6 ETC...
   for (let i = 1; i <= 6; i++) {
-    const image = document.createElement("picture");
-    image.setAttribute("id", "imageId");
-    image.innerHTML = `<img class="projectPage" src="./images/${i}.png" />`;
-    gallery.appendChild(image);
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+    container.innerHTML = `<video class="projectPage" src="./videoGif/${i}.mp4" loop autoplay muted></video>`;
+
+    // const container1 = document.querySelector(".container");
+    // const secondDiv = document.createElement("div");
+    // secondDiv.setAttribute("class", "btnPop");
+    // secondDiv.innerText = "view project";
+    // container1.appendChild(secondDiv);
+
+    container.onclick = function () {
+      showImage(i);
+    };
+    gallery.appendChild(container);
   }
 }
 
-const imageId = document.getElementById("imageId");
-imageId.addEventListener("mouseover", projectBtn());
+// FUNCTION TO CREATE A MODAL
+function showImage(imageNumber) {
+  const video = document.createElement("DIV");
+  video.setAttribute("class", "divContainVideo");
+  video.innerHTML = `<video class="videoModalStyle" src="./videoGif/${imageNumber}.mp4" loop autoplay muted></video>`;
 
-function projectBtn() {
-  const createBtn = document.createElement("div");
-  createBtn.innerText = "View Project";
-  imageId.appendChild(createBtn);
+  // create the overlay
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(video);
+  overlay.classList.add("overlay");
+
+  // you can close the modal image just click anywhere
+  overlay.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove(); // quita el haber abierto la imagen.
+  };
+  // modal close botton
+  const closeModalBotton = document.createElement("P");
+  closeModalBotton.textContent = "X";
+  closeModalBotton.classList.add("close-btn");
+
+  //click-to-close action.
+  closeModalBotton.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body"); // quita el no poder dar scroll una vez cerrada la imagen
+    overlay.remove(); // quita el haber abierto la imagen.
+  };
+
+  overlay.appendChild(closeModalBotton);
+
+  //add to html
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
