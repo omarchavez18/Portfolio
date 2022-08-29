4629a0a5e49678ff739fcb159c1e255680399a14
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-08-29 01:19:06 -0500

    create modal elements

--- app.js
+++ app.js
@@ -39,24 +39,32 @@ function createGallery() {
     container.setAttribute("class", "container");
     container.innerHTML = `<video class="projectPage" src="./videoGif/${i}.mp4" loop autoplay muted></video>`;
 
-    // const container1 = document.querySelector(".container");
-    // const secondDiv = document.createElement("div");
-    // secondDiv.setAttribute("class", "btnPop");
-    // secondDiv.innerText = "view project";
-    // container1.appendChild(secondDiv);
-
     container.onclick = function () {
-      showImage(i);
+      showVideo(i);
     };
     gallery.appendChild(container);
   }
 }
 
 // FUNCTION TO CREATE A MODAL
-function showImage(imageNumber) {
+function showVideo(imageNumber) {
   const video = document.createElement("DIV");
-  video.setAttribute("class", "divContainVideo");
+  video.setAttribute("class", "divContainVideo flex2Columns");
   video.innerHTML = `<video class="videoModalStyle" src="./videoGif/${imageNumber}.mp4" loop autoplay muted></video>`;
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "modalDiv");
+  newDiv.innerHTML = `<p>here we are gonna put the information about the project and the techmologies that we use and a couple of bottons to link to the code and the output</p>`;
+  video.appendChild(newDiv);
+
+  const btnPage = document.createElement("button");
+  btnPage.setAttribute("class", "btnPage");
+  btnPage.innerHTML = `<a  href="https://dshagui.com/#home"><p> View Website</p></a>`;
+  const btnCode = document.createElement("button");
+  btnCode.setAttribute("class", "btnPage");
+  btnCode.innerHTML = `<a  href="https://jacekjeznach.com/"><p>View Code</p></a>`;
+
+  newDiv.appendChild(btnPage);
+  newDiv.appendChild(btnCode);
 
   // create the overlay
   const overlay = document.createElement("DIV");
