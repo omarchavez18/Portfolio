d7753bc77ec002e82222a91d622b5a1a1ea0e4c7
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-08-30 19:30:40 -0500

    array of projects added, arrayProjects added to the createGallery function using a map

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
+const arrayProjects = [
+  {
+    name: "hunting app",
+    description: "este es mi proyecto de caza",
+    video: 1,
+    technologies: [
+      "javascript",
+      "html",
+      "scss, heroku, jest, express, node js, postman ",
+    ],
+    liveDemo: "https://omarchavez18.github.io/Hunting-App/",
+    repository: "https://github.com/omarchavez18/Hunting-App",
+  },
+  {
+    name: "PokeApi",
+    description: "este es mi proyecto de pokeApi",
+    video: 2,
+    technologies: ["javascript", "html", "css"],
+    liveDemo: "https://omarchavez18.github.io/PokeApi-Microsoft-Launch-X/",
+    repository: "https://github.com/omarchavez18/PokeApi-Microsoft-Launch-X",
+  },
+  {
+    name: "EDM festival",
+    description: "este es mi proyecto de EDM festival",
+    video: 3,
+    technologies: ["javascript", "html", "scss"],
+    liveDemo: "https://omarchavez18.github.io/festival-de-musica/",
+    repository: "https://github.com/omarchavez18/festival-de-musica",
+  },
+  {
+    name: "Vaccines",
+    description: "este es mi proyecto de clon de pagina de vacunacion ",
+    video: 4,
+    technologies: ["html", "scss"],
+    liveDemo:
+      "https://omarchavez18.github.io/FrontEnd-Mission-CSS-Microsoft-Launch-X/",
+    repository:
+      "https://github.com/omarchavez18/FrontEnd-Mission-CSS-Microsoft-Launch-X",
+  },
+  {
+    name: "TNW",
+    description: "este es mi proyecto de clon de pagina de noticias TNW",
+    video: 5,
+    technologies: ["html", "css"],
+    liveDemo: "https://omarchavez18.github.io/TNW-proyect/",
+    repository: "https://github.com/omarchavez18/TNW-proyect",
+  },
+  {
+    name: "Coffee blog",
+    description: "este es mi proyecto de blog de cafe",
+    video: 6,
+    technologies: ["html", "css"],
+    liveDemo: "https://omarchavez18.github.io/coffee-blog/",
+    repository: "https://github.com/omarchavez18/coffee-blog",
+  },
+];
+
 toggle = document.querySelectorAll(".toggle")[0];
 nav = document.querySelectorAll("nav")[0];
 toggle_open_text = "Menu";
@@ -34,34 +91,39 @@ function createGallery() {
   const gallery = document.querySelector(".project");
 
   // FOR THIS SECTION THE NAME OF YOUR IMAGES MOST BE NUMBERS = 1,2,3,4,5,6 ETC...
-  for (let i = 1; i <= 6; i++) {
+
+  arrayProjects.map((proyecto) => {
     const container = document.createElement("div");
     container.setAttribute("class", "container");
-    container.innerHTML = `<video class="projectPage" src="./videoGif/${i}.mp4" loop autoplay muted></video>`;
-
+    container.innerHTML = `<video class="projectPage" src="./videoGif/${proyecto.video}.mp4" loop autoplay muted></video>`;
+    // create view project btn
+    const secondDiv = document.createElement("div");
+    secondDiv.setAttribute("class", "btnPop");
+    secondDiv.innerText = "view project";
+    container.appendChild(secondDiv);
     container.onclick = function () {
-      showVideo(i);
+      showModal(proyecto);
     };
     gallery.appendChild(container);
-  }
+  });
 }
 
 // FUNCTION TO CREATE A MODAL
-function showVideo(imageNumber) {
+function showModal(proyecto) {
   const video = document.createElement("DIV");
   video.setAttribute("class", "divContainVideo flex2Columns");
-  video.innerHTML = `<video class="videoModalStyle" src="./videoGif/${imageNumber}.mp4" loop autoplay muted></video>`;
+  video.innerHTML = `<video class="videoModalStyle" src="./videoGif/${proyecto.video}.mp4" loop autoplay muted></video>`;
   const newDiv = document.createElement("div");
   newDiv.setAttribute("class", "modalDiv");
-  newDiv.innerHTML = `<p>here we are gonna put the information about the project and the techmologies that we use and a couple of bottons to link to the code and the output</p>`;
+  newDiv.innerHTML = `<p>${proyecto.description}</p>`;
   video.appendChild(newDiv);
 
   const btnPage = document.createElement("button");
   btnPage.setAttribute("class", "btnPage");
-  btnPage.innerHTML = `<a  href="https://dshagui.com/#home"><p> View Website</p></a>`;
+  btnPage.innerHTML = `<a  href="${proyecto.liveDemo}" target= "_blank"><p> View Website</p></a>`;
   const btnCode = document.createElement("button");
   btnCode.setAttribute("class", "btnPage");
-  btnCode.innerHTML = `<a  href="https://jacekjeznach.com/"><p>View Code</p></a>`;
+  btnCode.innerHTML = `<a  href="${proyecto.repository}" target= "_blank"><p>View Code</p></a>`;
 
   newDiv.appendChild(btnPage);
   newDiv.appendChild(btnCode);
